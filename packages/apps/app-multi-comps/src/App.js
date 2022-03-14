b1be8881bfbc080a3fb0b765fb8e8e0886626cfe
kmorgan <kiki.morgan13@gmail.com>
2022-03-14 00:58:44 -0400

    minor change + medium vuln

--- packages/apps/app-multi-comps/src/App.js
+++ packages/apps/app-multi-comps/src/App.js
@@ -33,6 +33,7 @@ class App extends Component {
           <div className="components">
             <CompOne />
             <CompTwo />
+            <CompOne />
           </div>
         </header>
       </div>
